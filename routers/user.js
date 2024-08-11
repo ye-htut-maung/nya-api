cc5ec6d3394408824628b73641bd8bdbd9231d8a
ye-htut-maung <yehtutmaung21@gmail.com>
2024-08-11 14:51:10 -0400

    Created APIs

---
+++ routers/user.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const prisma = require("../prismaClient");
+const bcrypt = require("bcrypt");
+
+router.get("/users", async (req, res) => {
+  try {
+    const data = await prisma.user.findMany({
+      include: { posts: true, comments: true },
+      orderBy: { id: "desc" },
+      take: 20,
+    });
+    res.join(data);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await params.user.findFirst({
+      where: { id: Number(id) },
+      include: { posts: true, comments: true },
+    });
+
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.post("/users", async (req, res) => {
+  const { name, username, bio, password } = req.body;
+  if (!name || !username || !password) {
+    return res
+      .status(400)
+      .json({ msg: "name, username and password required" });
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: { name, username, password: hash, bio },
+  });
+  res.json(user);
+});
+
+module.exports = { userRouter: router };
